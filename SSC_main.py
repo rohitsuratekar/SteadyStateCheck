25150f71689d9b9ba071b66191d3f251830f66b5
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-08-24 17:18:55 +0530

    Ready with loops. You can define each parameter range now.

--- SSC_main.py
+++ SSC_main.py
@@ -10,78 +10,87 @@ from scipy.integrate import odeint
 #matplotlib.use('TkAgg') #For interactive plots
 #import matplotlib.pyplot as plt
 #Setting up Vmax table (These are all free parameters)
-v2 = 1.0 #PI4K value
-v5 = 9.0#DGK value
-v7 = 1.0 #CDPDAGS value
-v8 = 10.0 #PIS value
 v_reversible = 0.01 #all reversible backword reaction rates
-vmax_table = [v2,v5,v7,v8,v_reversible] #Rest of Vmax will be set according to these values
+#Specify Range
+v2_range  = np.linspace(1, 10, 2)
+v5_range  = np.linspace(1, 10, 2)
+v7_range  = np.linspace(1, 10, 2)
+v8_range  = np.linspace(1, 10, 2)
 
-y = SSC_initial_conditions.initial_conditions_vector()     # initial condition vector
-time_coord = SSC_initial_conditions.time_conditions()
-t  = np.linspace(time_coord[0], time_coord[1], 10000)   # time grid
-soln = odeint(SSC_ode_function.ode_function, y, t , args=(vmax_table,))
-pPMPI = soln[:, 0]
-pPI4P = soln[:, 1]
-pPIP2 = soln[:, 2]
-pDAG= soln[:, 3]
-pPMPA= soln[:, 4]
-pERPA= soln[:, 5]
-pCDPDAG= soln[:, 6]
-pERPI= soln[:, 7]
+for temp_v2 in v2_range:
+    for temp_v5 in v5_range:
+        for temp_v7 in v7_range:
+            for temp_v8 in v8_range:
+                            v5 = temp_v5
+                            v2 = temp_v2
+                            v7 = temp_v7
+                            v8 = temp_v8
+                            vmax_table = [v2,v5,v7,v8,v_reversible] #Rest of Vmax will be set according to these values
+                            y = SSC_initial_conditions.initial_conditions_vector()     # initial condition vector
+                            time_coord = SSC_initial_conditions.time_conditions()
+                            t  = np.linspace(time_coord[0], time_coord[1], 10000)   # time grid
+                            soln = odeint(SSC_ode_function.ode_function, y, t , args=(vmax_table,))
+                            pPMPI = soln[:, 0]
+                            pPI4P = soln[:, 1]
+                            pPIP2 = soln[:, 2]
+                            pDAG= soln[:, 3]
+                            pPMPA= soln[:, 4]
+                            pERPA= soln[:, 5]
+                            pCDPDAG= soln[:, 6]
+                            pERPI= soln[:, 7]
 
-all_concentrations_main = soln[-1,:]  #Get all final concentrations
-total_concentration_main = sum(all_concentrations_main)  #Total Concentration
+                            all_concentrations_main = soln[-1,:]  #Get all final concentrations
+                            total_concentration_main = sum(all_concentrations_main)  #Total Concentration
 
-cPMPI = all_concentrations_main[0]
-cPI4P = all_concentrations_main[1]
-cPIP2 = all_concentrations_main[2]
-cDAG= all_concentrations_main[3]
-cPMPA= all_concentrations_main[4]
-cERPA= all_concentrations_main[5]
-cCDPDAG= all_concentrations_main[6]
-cERPI= all_concentrations_main[7]
+                            cPMPI = all_concentrations_main[0]
+                            cPI4P = all_concentrations_main[1]
+                            cPIP2 = all_concentrations_main[2]
+                            cDAG= all_concentrations_main[3]
+                            cPMPA= all_concentrations_main[4]
+                            cERPA= all_concentrations_main[5]
+                            cCDPDAG= all_concentrations_main[6]
+                            cERPI= all_concentrations_main[7]
 
-other_than_erpi = [ cPMPI, cPI4P, cPIP2, cDAG, cPMPA, cERPA, cCDPDAG ]
-total_pi = cPMPI + cERPI
-total_pa = cPMPA + cERPA
-other_than_cdpdag = [ total_pa, total_pi, cPI4P, cPIP2 ]
+                            other_than_erpi = [ cPMPI, cPI4P, cPIP2, cDAG, cPMPA, cERPA, cCDPDAG ]
+                            total_pi = cPMPI + cERPI
+                            total_pa = cPMPA + cERPA
+                            other_than_cdpdag = [ total_pa, total_pi, cPI4P, cPIP2 ]
 
 
-j1 = [i1 for i1 in all_concentrations_main if i1 < 0 ] #Checking for negative values
-j2 = [i2 for i2 in other_than_erpi if i2 > cERPI ] #Checking Values greater than ERPI
-j3 = [i3 for i3 in other_than_cdpdag if i3 < cCDPDAG ] #Checking Values less than CDPDAG
+                            j1 = [i1 for i1 in all_concentrations_main if i1 < 0 ] #Checking for negative values
+                            j2 = [i2 for i2 in other_than_erpi if i2 > cERPI ] #Checking Values greater than ERPI
+                            j3 = [i3 for i3 in other_than_cdpdag if i3 < cCDPDAG ] #Checking Values less than CDPDAG
 
-penalty_error = 0
+                            penalty_error = 0
 
-if len(j1) == 0:
-    penalty_error = penalty_error -400
-if len(j2) == 0:
-    penalty_error = penalty_error -10
-if len(j3) == 0:
-    penalty_error = penalty_error -5
+                            if len(j1) == 0:
+                                penalty_error = penalty_error -400
+                            if len(j2) == 0:
+                                penalty_error = penalty_error -10
+                            if len(j3) == 0:
+                                penalty_error = penalty_error -5
 
-#Error checking
-rpip2=cPIP2/(cERPI+cPMPI) #Should be 0.05
-rpi4p=cPI4P/(cERPI+cPMPI)  #Should be 0.05
-rpa= (cPMPA+cERPA)/(cERPI+cPMPI) #Should be 0.167
-rdag= (cDAG)/(cERPI+cPMPI) #Should be 0.008
+                            #Error checking
+                            rpip2=cPIP2/(cERPI+cPMPI) #Should be 0.05
+                            rpi4p=cPI4P/(cERPI+cPMPI)  #Should be 0.05
+                            rpa= (cPMPA+cERPA)/(cERPI+cPMPI) #Should be 0.167
+                            rdag= (cDAG)/(cERPI+cPMPI) #Should be 0.008
 
-error_from_pip2 = (math.fabs(rpip2-0.05))/0.05
-error_from_pi4p = (math.fabs(rpi4p-0.05))/0.05
-error_from_pa = (math.fabs(rpa-0.167))/0.167
-error_from_dag = (math.fabs(rdag-0.008))/0.008
+                            error_from_pip2 = (math.fabs(rpip2-0.05))/0.05
+                            error_from_pi4p = (math.fabs(rpi4p-0.05))/0.05
+                            error_from_pa = (math.fabs(rpa-0.167))/0.167
+                            error_from_dag = (math.fabs(rdag-0.008))/0.008
 
-ferror = error_from_pip2 + error_from_pi4p + error_from_pa + error_from_dag
-error_details = [error_from_pip2 ,error_from_pi4p, error_from_pa, error_from_dag]
-#Adding Penalty Error
-ferror = ferror + 415 + penalty_error
+                            ferror = error_from_pip2 + error_from_pi4p + error_from_pa + error_from_dag
+                            error_details = [error_from_pip2 ,error_from_pi4p, error_from_pa, error_from_dag]
+                            #Adding Penalty Error
+                            ferror = ferror + 415 + penalty_error
 
-#Saving results
-fh2 = open('current_result_log.txt','a')
-write_con = np.array(all_concentrations_main).tolist()
-things_to_write = [ferror] + vmax_table + error_details + write_con
-things_to_write = [ float(round(elem1,3)) for elem1 in things_to_write ]
-fh2.write('\t'.join(str(k1) for k1 in things_to_write))
-fh2.write('\n')
-fh2.close()
+                            #Saving results
+                            fh2 = open('current_result_log.txt','a')
+                            write_con = np.array(all_concentrations_main).tolist()
+                            things_to_write = [ferror] + vmax_table + error_details + write_con
+                            things_to_write = [ float(round(elem1,3)) for elem1 in things_to_write ]
+                            fh2.write('\t'.join(str(k1) for k1 in things_to_write))
+                            fh2.write('\n')
+                            fh2.close()

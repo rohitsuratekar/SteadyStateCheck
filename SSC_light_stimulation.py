c5b8c42c2e67d752e1299f8940d9415b9a7824ca
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-08-31 17:26:07 +0530

    Complete code which stores 90% recovery. If it has not happened in given time, it will keep time period as 1989

--- SSC_light_stimulation.py
+++ SSC_light_stimulation.py
@@ -7,7 +7,7 @@ import SSC_initial_conditions, SSC_ode_function, SSC_ode_function, SSC_parameter
 import numpy as np
 from scipy.integrate import odeint
 #Parameter initializing
-stimulation_factor = 1000 #this will be change in PLC Vmax
+stimulation_factor = 1000.00 #this will be change in PLC Vmax
 
 #Open parameter values which gave correct results
 with open("temp_file.txt") as original_parameters:
@@ -31,7 +31,7 @@ for para_elements in range(len(para_set)):
     time_coord = SSC_initial_conditions.time_conditions()
     t  = np.linspace(time_coord[0], time_coord[1], 10000)   # time grid
     soln = odeint(SSC_ode_function.ode_function, y, t , args=(vmax_table,))
-
+    stored_pip2_value = soln[-1,2]
     #Light Stimulation
     y1 = soln[-1,:]  #Get all final concentrations, This will initial condition for next step
     t1 = np.linspace(time_coord[0], time_coord[2], 100)
@@ -39,7 +39,20 @@ for para_elements in range(len(para_set)):
 
     #Recovery phase
     y2 = stimulus_soln[-1,:]
-
-    print (y2[2]/y1[2])*100
     t2 = np.linspace(time_coord[0], time_coord[3], 10000)
     recovery_soln = odeint(SSC_ode_function.ode_function, y2, t2 , args=(vmax_table,))
+
+    pip2_recovery = recovery_soln[:,2]
+    percent90_value = stored_pip2_value*0.9
+    recovery_time_array = t2 [pip2_recovery > percent90_value]
+    if len(recovery_time_array) == 0:
+        final_recovery_time = 1989
+    if len(recovery_time_array) != 0:
+        final_recovery_time = recovery_time_array[0]
+
+    mfh2 = open('light_recovery.txt','a')
+    things_to_write2 = vmax_table + [final_recovery_time] + [stimulation_factor] + [time_coord[2]]
+    things_to_write2 = [ float(round(elem1,3)) for elem1 in things_to_write2 ]
+    mfh2.write('\t'.join(str(k1) for k1 in things_to_write2))
+    mfh2.write('\n')
+    mfh2.close()

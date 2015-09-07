c005702fcae82ae0edef8bb12ed0d57cf3d1de36
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-09-07 11:04:09 +0530

    Added support to double mutant files

--- plotting_functions.py
+++ plotting_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-open_normal = 2  #0 = mutant , 1 = Normal , 2 = light
+open_normal = 3  #0 = mutant , 1 = Normal , 2 = light, double mutant
 
 if open_normal == 1:
     with open("current_result_log.txt") as original_parameters:
@@ -53,14 +53,14 @@ if open_normal == 0:
     i8 = k1[:,22]/k1[:,14]
     m1 = (k1[:,19]+k1[:,20])/(k1[:,11]+k1[:,12])
     m2 = (k1[:,15]+k1[:,22])/(k1[:,7]+k1[:,14])
-    plt.hist(m1,100, alpha=0.5, label='Total PA ')
+    #plt.hist(m1,100, alpha=0.5, label='Total PA ')
     #plt.hist(m2,100, alpha=0.5, label='Total PI ')
     #plt.hist(i1,100, alpha=0.5, label='PMPI ')
     #plt.hist(i2,100, alpha=0.5, label='PI4P ')
-    #plt.hist(i3,100, alpha=0.5, label='PIP2 ')
-    #plt.hist(i4,100, alpha=0.5, label='DAG')
-    plt.hist(i5,100, alpha=0.5, label='PMPA ')
-    plt.hist(i6,100, alpha=0.5, label='ERPA ')
+    plt.hist(i3,100, alpha=0.5, label='PIP2 ')
+    plt.hist(i4,100, alpha=0.5, label='DAG')
+    #plt.hist(i5,100, alpha=0.5, label='PMPA ')
+    #plt.hist(i6,100, alpha=0.5, label='ERPA ')
     #plt.hist(i7,100, alpha=0.5, label='CDPDAG ')
     #plt.hist(i8,100, alpha=0.5, label='ERPI ')
 
@@ -79,9 +79,38 @@ if open_normal == 2:
     plt.hist(k16[:,6],100, alpha=0.5, label='90 %')
     plt.hist(ksmall[:,6],100, alpha=0.5, label='99 %')
 
-plt.xlabel('Recovery Time (min)')
+if open_normal == 3:
+    with open("mutant_log.txt") as original_parameters:
+        k2 = [[float(digit) for digit in line.split()] for line in original_parameters]
+    k2 = np.asarray(k2)
+    #k2 = k2[k2[:,6] < 1000 , :] #Mutant factor less than this value
+    k2 = k2[k2[:,6] != 1 , :] #Enzyme 1
+    k2 = k2[k2[:,8] != 1 , :] #Enzyme 2
+    k1 = k2
+    i1 = k1[:,17]/k1[:,9]
+    i2 = k1[:,18]/k1[:,10]
+    i3 = k1[:,19]/k1[:,11]
+    i4 = k1[:,20]/k1[:,12]
+    i5 = k1[:,21]/k1[:,13]
+    i6 = k1[:,22]/k1[:,14]
+    i7 = k1[:,23]/k1[:,15]
+    i8 = k1[:,24]/k1[:,16]
+    m1 = (k1[:,21]+k1[:,22])/(k1[:,13]+k1[:,14])
+    m2 = (k1[:,17]+k1[:,24])/(k1[:,9]+k1[:,16])
+    #plt.hist(m1,100, alpha=0.5, label='Total PA ')
+    #plt.hist(m2,100, alpha=0.5, label='Total PI ')
+    #plt.hist(i1,100, alpha=0.5, label='PMPI ')
+    #plt.hist(i2,100, alpha=0.5, label='PI4P ')
+    plt.hist(i3,100, alpha=0.5, label='PIP2 ')
+    plt.hist(i4,100, alpha=0.5, label='DAG')
+    #plt.hist(i5,100, alpha=0.5, label='PMPA ')
+    #plt.hist(i6,100, alpha=0.5, label='ERPA ')
+    #plt.hist(i7,100, alpha=0.5, label='CDPDAG ')
+    #plt.hist(i8,100, alpha=0.5, label='ERPI ')
+
+plt.xlabel('Ratio os steady states ( PITP-PATP/WT)')
 plt.ylabel('Frequency')
-plt.title('Recovery with different amount of PIP2 depletion')
+plt.title('PITP-PATP mutant')
 #plt.axhline()
 #plt.axvline()
 plt.legend(loc='upper left', bbox_to_anchor=(1, 0.5))
